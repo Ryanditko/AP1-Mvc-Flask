205a32ecd311f0876b6fd2a2c113b5a54b0e47e5
Ryan Rodrigues Cordeiro <yryurodriguess@gmail.com>
2025-09-28 18:23:20 -0300

    Update ctrls_turma.py

--- src/controllers/ctrls_turma.py
+++ src/controllers/ctrls_turma.py
@@ -6,6 +6,6 @@ class AlunoController:
      # A chamada para esse método seria feita diretamente pela classe, sem a necessidade de criar um objeto (uma instância):
      @staticmethod
      def index():
-          turmas = turmas.query.all()
+          turmas = Turma.query.all()
         #  validar pois no enunciado indica que não será usado views 
           return render_template('', turmas=turmas)
