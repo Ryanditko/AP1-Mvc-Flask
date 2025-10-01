c70266927ee2c5ed8556e41b937b087a2fa7aa0f
Felipe Viana <felipe.wilson@aluno.faculdadeimpacta.com.br>
2025-09-30 23:03:53 -0300

    feat: atualizando o main.py com import do Swagger, requirements.txt com instalação do flasgger e criando swagger.yaml para documentação

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 from flask import Flask
 from config import Config
 from models import banco_de_dados
+from flasgger import Swagger
 from api.professores.api_professores import rotas_professores
 
 # TODO configurar a aplicação flask aqui abaixo (sem a necessidade de views/html para a aplicação)
@@ -15,9 +16,12 @@ app.config.from_object(Config)
 # iniciando o banco de dados 
 banco_de_dados.init_app(app)
 
+swagger = Swagger(app, template_file='docs/swagger.yaml')
+
 with app.app_context():
     banco_de_dados.create_all()
 
 rotas_professores(app)
+
 if __name__ == '__main__':
     app.run(debug=True)
