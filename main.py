336cb35a4b8fc8f413adf276a2137c433019ec00
Felipe Wilson Viana <167330083+Felipewv93@users.noreply.github.com>
2025-10-03 17:37:45 -0300

    Configure Flask app to use environment variables

--- main.py
+++ main.py
@@ -30,4 +30,8 @@ rotas_turmas(app)
 rotas_aluno(app)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    host = os.environ.get('HOST', '0.0.0.0')
+    debug = os.environ.get('FLASK_ENV') != 'production'
+    
+    app.run(host=host, port=port, debug=debug)
